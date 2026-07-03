a34c34b743313e53899edb4b4fd5badc9d67dfe8
DeysonSantana <deyson.s.jesus@hotmail.com>
2026-07-03 15:44:53 -0300

    Endereço da API em produção

--- src/frontend/assets/js/api.js
+++ src/frontend/assets/js/api.js
@@ -3,7 +3,9 @@
  * Este arquivo centraliza todas as chamadas HTTP para o servidor Node.js.
  */
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'https://stockvision-qn9r.onrender.com/api';
+
+// const BASE_URL = 'http://localhost:3000/api';
 
 /**
  * 💾 GERENCIADOR DE SESSÃO LOCAL (LocalStorage)
